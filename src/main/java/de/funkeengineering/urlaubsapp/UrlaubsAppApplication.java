e5abfe42c3c6967c9b22fa9b7d4dcf2b8995d368
Wojciech Mysliwiec <wojciech.mysliwiec@funke-engineering.de>
2023-11-03 08:31:19 +0100

    init commit

---
+++ src/main/java/de/funkeengineering/urlaubsapp/UrlaubsAppApplication.java
@@ -0,0 +1,13 @@
+package de.funkeengineering.urlaubsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlaubsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlaubsAppApplication.class, args);
+    }
+
+}
